d66ceb9fec0e7c1568ba0f8f8faee050a29eb52b
Craig <craig.anderson0@gmail.com>
2017-02-17 13:42:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var trello;
 
 exports.handler = function(event, context, callback) {
   var accessToken = event.session.user.accessToken;
-  trello = new Trello('08803da31ca2853f1c7413325e7db8eb', accessToken);
+  trello = new Trello(process.env.APP_KEY, accessToken);
   var alexa = Alexa.handler(event, context);
   alexa.registerHandlers(handlers);
   alexa.execute();
